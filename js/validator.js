4dbbae8cbd0a96e080c8648a96998ca4db26477d
vk0808 <vk4765712@gmail.com>
2021-09-21 21:43:07 +0530

    feat: add event listener for salary slider and update o/p

---
+++ js/validator.js
@@ -0,0 +1,12 @@
+function salaryOutput() {
+  const salary = document.querySelector('#salary');
+  const output = document.querySelector('.salary-output');
+  output.textContent = salary.value;
+  salary.addEventListener('input', function () {
+    output.textContent = salary.value;
+  });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  salaryOutput();
+});
