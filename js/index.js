00f13ea27f2e3402faad0134f162d816a3f055bb
vk0808 <vk4765712@gmail.com>
2021-09-22 13:00:18 +0530

    feat: display table by reading data from JSON

--- js/index.js
+++ js/index.js
@@ -7,23 +7,68 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const createInnerHtml = () => {
   // column headings
   const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th>" + "<th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
-  // data row
-  const innerHtml = `${headerHtml}
-    <tr>
-      <td><img class="profile" src="../assets/profile-images/Ellipse -2.png" alt=""></td>
-      <td>Surya Kumar</td>
-      <td>Male</td>
-      <td>
-        <div class="dept-label">HR</div>
-        <div class="dept-label">Finance</div>
-      </td>
-      <td>300000</td>
-      <td>1 Nov 2008</td>
-      <td>
-        <img src="../assets/icons/delete-black-18dp.svg" alt="delete">
-        <img src="../assets/icons/create-black-18dp.svg" alt="create">
-      </td>
-    </tr>`
-    ;
+  // add table header 
+  let innerHtml = `${headerHtml}`;
+  // read from JSON object
+  let empPayrollList = createEmployeePayrollJSON();
+  // data row loop through JSON object
+  for (let empPayrollData of empPayrollList) {
+    // append row to the existing rows
+    innerHtml = `${innerHtml}
+      <tr>
+        <td><img class="profile" alt="${empPayrollData._name}" src ="${empPayrollData._profilePic}"></td>
+        <td>${empPayrollData._name}</td>
+        <td>${empPayrollData._gender}</td>
+        <td>${getDepartmentHtml(empPayrollData._department)}</td>
+        <td>${empPayrollData._salary}</td>
+        <td>${empPayrollData._startDate}</td>
+        <td>
+          <img id ="${empPayrollData._id}" src="../assets/icons/delete-black-18dp.svg" alt="Delete" onClick="remove(this)">
+          <img id ="${empPayrollData._id}" src="../assets/icons/create-black-18dp.svg" alt="Edit" onClick="update(this)">
+        </td>
+      </tr>`
+      ;
+  }
   document.querySelector('#display').innerHTML = innerHtml;
+}
+
+// function to return JSON object
+const createEmployeePayrollJSON = () => {
+  let empPayrollListLocal = [
+    {
+      _name: 'Surya Kumar',
+      _gender: 'Male',
+      _department: [
+        'HR',
+        'Finance'
+      ],
+      _salary: '400000',
+      _startDate: '2 Aug 2021',
+      _note: '',
+      _id: new Date().getTime(),
+      _profilePic: '../assets/profile-images/Ellipse -2.png'
+    },
+    {
+      _name: 'Punith Kumar',
+      _gender: 'Male',
+      _department: [
+        'Engineer'
+      ],
+      _salary: '450000',
+      _startDate: '12 Sep 2021',
+      _note: '',
+      _id: new Date().getTime(),
+      _profilePic: '../assets/profile-images/Ellipse -3.png'
+    }
+  ];
+  return empPayrollListLocal;
+}
+
+// function to get department from array and create label for each
+const getDepartmentHtml = (data) => {
+  let deptHtml = '';
+  for (let dept of data) {
+    deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`;
+  }
+  return deptHtml;
 }
